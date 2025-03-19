7f5197f27048ebac96f7552c72b4aecad0126868
Anderson Fontes Fernandes Júnior <andersonfontes795@gmail.com>
2025-03-19 19:13:17 -0300

    Add files via upload

---
+++ Atividade Desenvolvimento Web - Props/front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Exercise1 from './componentes/ex1';
+import Exercise2 from './componentes/ex2';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Exercise1 />
+      <Exercise2 />
+    </div>
+  );
+};
+
+export default App;
